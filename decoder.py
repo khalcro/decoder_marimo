e25825ab1b0efb03189013665870261d115d8b96
keithhalcro <keithhalcro+github@gmail.com>
2026-07-30 19:56:45 -0600

    added csv upload

--- decoder.py
+++ decoder.py
@@ -21,7 +21,17 @@ def _():
 
 
 @app.cell
-def _(Enum, mo, pl):
+def _():
+    # TODO
+    ## upload csv from ST decoder  DONE
+    ## parse existing dataset for locales
+    ## plot multiple locales against each other
+    ## handle rounding value
+    return
+
+
+@app.cell
+def _(Enum, can_df, mo, pl, us_df):
     # setup the inputs
 
     _can_form_dict = {
@@ -89,7 +99,7 @@ def _(Enum, mo, pl):
         calculated_cols = {key: pl.Int32 for key in ['total_assets', 'total_liabilities', 'net_position', 'total_revenues', 'total_government_transfers', 'net_financial_position']}
         ratio_cols = {key: pl.Float32 for key in ['financial_assets-to-liabilities', 'assets-to-liabilities', 'net_debt-to-total-revenues', 'interest-to-total_revenues', 'net_book-to-cost_of_tca', 'govt_transfers-to-total_revenues']}
         metadata_cols = {'rounding': pl.String}
-    
+
         schema = descriptive_cols | input_cols | calculated_cols | ratio_cols | metadata_cols
 
         class Country(Enum):
@@ -97,28 +107,28 @@ def _(Enum, mo, pl):
             US = 2
 
         data = {}
-    
+
         def determine_country(self, country_str: str):
             self.country = getattr(self.Country, country_str.upper())
 
         def retype_data(self):
             self.data.update({key: int(val) for key, val in self.data.items() if self.schema[key] != pl.String})
-    
+
         def calculate_summaries(self):
             self.data['total_assets'] = self.data['current_assets'] + self.data['capital_assets']
-        
+    
             if self.country is self.Country.CANADA:
                 self.data['total_liabilities'] = self.data['liabilities']
                 self.data['net_position'] = self.data['total_assets'] - self.data['total_liabilities']
                 self.data['total_revenues'] = self.data['revenues_total'] + self.data['other']
                 self.data['total_government_transfers'] = self.data['government_transfers'] + self.data['government_transfers_related_to_capital']
-            
+        
             if self.country is self.Country.US:
                 self.data['total_liabilities'] = self.data['liabilities'] + self.data['deferred_inflows']
                 self.data['net_position'] = self.data['total_assets'] + self.data['deferred_outflows'] - self.data['total_liabilities']
                 self.data['total_government_transfers'] = self.data['operating_grants_and_contributions'] + self.data['capital_grants_and_contributions']
                 self.data['total_cost_of_tca'] = self.data['govt_assets_not_depreciated'] + self.data['govt_assets_being_depreciated'] + self.data['govt_other_assets'] + self.data['bus_assests_not_depreciated'] + self.data['bus_assets_being_depreciated'] + self.data['bus_other_assets']
-            
+        
         def calculate_indicators(self):
             self.data['net_financial_position'] = self.data['current_assets'] - self.data['total_liabilities']
             def safe_divide(n, d):  # catch divide by zero
@@ -141,18 +151,59 @@ def _(Enum, mo, pl):
             self.retype_data()
             self.calculate_summaries()
             self.calculate_indicators()
-            breakpoint()
             self.data.update({key: None for key in self.schema if key not in self.data})
             return pl.DataFrame(data=self.data, schema=self.schema, strict=False)
-        
+    
     # setup dataframes with typing
     input_df = Entry().convert_to_dataframe(data={})
 
     def append_form_input(form):
         new_df = Entry().convert_to_dataframe(data=form)
-    
+
         global input_df
         input_df.vstack(new_df, in_place=True)
+
+    def browser_callback(filebrowser_form_input):
+        filebrowser = filebrowser_form_input['_input_section']
+        if len(filebrowser) > 0:
+            [read_csv(fb) for fb in filebrowser]
+
+    def read_csv(file_obj):
+        """
+        reads the input csv, and feeds into the existing framework
+        """
+        # common work
+        path = file_obj.path
+        df = pl.read_csv(path, has_header=False)  # we can just read the csv, it's pretty small
+        city = path.stem
+        rounding = df[2, 6]
+    
+        # check if second col contains "Canada" i.e. "N/A for Canada" -- this is maybe the best indicator I could find
+        if df.select(pl.col('column_2').str.contains('Canada').any()).item():
+            country = 'Canada'
+            subset_df = (
+                can_df[[4,6,7,10,14,15,17,18,20,21,22], 3:] # only the input data -- matches the keys in _can_form_dict
+                    .with_columns(pl.col(pl.Utf8).str.strip_chars())  # trim all whitespace in strings
+                    .with_columns(pl.col(pl.Utf8).str.replace_all(',', ''))  # remove commas (thousands sep)
+                    .with_columns(pl.all().cast(pl.Int64))  # cast to int
+            )
+            keys = ['country', 'city', 'year'] + [*_can_form_dict.keys()] + ['rounding']
+
+        else:
+             country = 'US'
+             subset_df = (
+                 us_df[[3,5,6,8,9,10,13,14,15,17,18,19,20,21,22,23,24], 4:] # only the input data -- matches the keys in _us_form_dict
+                     .with_columns(pl.col(pl.Utf8).str.strip_chars())  # trim all whitespace in strings
+                     .with_columns(pl.col(pl.Utf8).str.replace_all(',', ''))  # remove commas (thousands sep)
+                     .with_columns(pl.all().cast(pl.Int64))  # cast to int
+             )
+             keys = ['country', 'city', 'year'] + [*_us_form_dict.keys()] + ['rounding']
+
+        for cc in subset_df.iter_columns():
+            if not cc.is_null().any():  # check if we're missing any data
+                data = [country, city, *cc.to_list(), rounding]
+                append_form_input(dict(zip(keys, data)))
+
 
     _can_form = _make_form_markdown('can').batch(
         country=mo.ui.text(label='Country', value='Canada', disabled=True),
@@ -170,7 +221,16 @@ def _(Enum, mo, pl):
         rounding=mo.ui.radio(options=['dollars', 'thousands', 'millions'], value='dollars', label='Report Rounding', inline=True)
     ).form(show_clear_button=True, bordered=False, on_change=append_form_input)
 
-    input_section = mo.accordion({'Input new': mo.ui.tabs({'Canada': _can_form, 'US': _us_form})})
+    _upload_form = mo.md(
+        """
+        Upload a spreadsheet downloaded from the strongtowns finance decoder worksheet in csv form
+        {_input_section}
+        """).batch(
+        _input_section=mo.ui.file_browser(filetypes=['.csv'], label='Select an exported csv. Filename will be used as the city/locale name.')).form(
+            bordered=False, on_change=browser_callback)
+
+    input_section = mo.accordion({'Input new': mo.ui.tabs({'Canada': _can_form, 'US': _us_form}),
+                                 'Upload ST csv file': _upload_form})
 
 
 
@@ -179,7 +239,6 @@ def _(Enum, mo, pl):
         existing_df_lazy = pl.scan_parquet(_repo_data_path)
     else:
         existing_df_lazy = pl.LazyFrame(schema=Entry().convert_to_dataframe(data={}).schema)
-
     return input_df, input_section
 
 
